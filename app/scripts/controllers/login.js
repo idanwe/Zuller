a5ed3d298e65341378282dcd223af507fa84b9c4
idanw <idanwe2@gmail.com>
2013-10-12 14:19:55 +0300

    refactor login page

--- app/scripts/controllers/login.js
+++ app/scripts/controllers/login.js
@@ -1,21 +1,18 @@
-/**
- * Created by Sa on 05/10/13.
- */
 'use strict';
 
 angular.module('Zuller')
-    .controller('LoginCtrl', ['$scope','$location', 'facebookSdk', 'sharedProperties',
-        function($scope, $location, facebookSdk, sharedProperties){
+  .controller('LoginCtrl', ['$scope','$location', 'facebookSdk',
+    function($scope, $location, facebookSdk){
+      facebookSdk.$on('fb_status_changed', function(event, status, fb_user_id, response){
+          if (status === 'connected')
+             $location.path('/question');
+      });
 
-            facebookSdk.$on('fb_status_changed', function(event, status, fb_user_id, response){
-                if (status === 'connected')
-                   $location.path('/question');
-            });
+      $scope.continue = function(){
+          $location.path('/question');
+      }
 
-            $scope.continue = function(){
-                $location.path('/question');
-            }
-            $scope.facebookLogin = function(){
-                facebookSdk.login();
-            }
-        }]);
+      $scope.facebookLogin = function(){
+          facebookSdk.login();
+      }
+    }]);
