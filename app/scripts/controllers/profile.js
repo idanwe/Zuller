7e6ef14e3c5e19059df2a1b920b3d20ec9a3d4c6
idanw <idanwe2@gmail.com>
2013-10-21 21:52:10 +0300

    basic profile view

--- app/scripts/controllers/profile.js
+++ app/scripts/controllers/profile.js
@@ -1,16 +1,25 @@
 angular.module('Zuller')
-    .controller('ProfileCtrl', ['$scope', 'facebookSdk', function($scope, facebookSdk) { // TODO: add User service
+    .controller('ProfileCtrl', ['$scope', 'facebookSdk', 'User', function($scope, facebookSdk, User) {
         $scope.templateUrl = 'views/profile.html';
         $scope.loggedIn = false;
         $scope.login = facebookSdk.login;
         $scope.logout = facebookSdk.logout;
+
+        $scope.have_musics = function() {
+          return User.favorite_music.length > 0;
+        };
+
+        $scope.have_beverages = function() {
+          return User.favorite_beverage.length > 0;
+        };
+
         facebookSdk.$on('fb_status_changed', function(event, status, fb_user_id, response) {
           $scope.loggedIn = status == "connected";
-          var promise = facebookSdk.getUser()
-          promise.then(function(res) {
-            var img_url = "http://graph.facebook.com/"+res.id+"/picture";
-            res.img_url = img_url;
-            $scope.user = res;
+
+          facebookSdk.getUser().then(function(res) {
+            res.img_url = "http://graph.facebook.com/"+res.id+"/picture";
+            $scope.res = res;
+            $scope.user = User;
           });
         })
     }]);
