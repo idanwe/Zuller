eb93f8b0ccb263f92997d05ace9cf1936914a304
Idan Wender <idanwe2@gmail.com>
2014-01-23 12:27:02 +0200

    fix new routes and libraries

--- app/scripts/services/constants.js
+++ app/scripts/services/constants.js
@@ -1,3 +1,3 @@
 angular.module('Zuller')
   .constant("facebookAppId", "501160893291995")
-  .constant("serverUrl", "http://zuller.herokuapp.com/api");
+  .constant("serverUrl", "http://zuller-rails.herokuapp.com/api");
